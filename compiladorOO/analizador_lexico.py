0e6ef155955838fcdf01792c76369e614619232d
Michel Oliveira <micheltdoliveira@gmail.com>
2022-03-26 14:04:13 -0300

    add verifica_palavras_reservadas

--- compiladorOO/analizador_lexico.py
+++ compiladorOO/analizador_lexico.py
@@ -1,4 +1,4 @@
-from token import Token
+from token_lex import TokenLex
 
 class AnalizadorLexico:
 
@@ -21,6 +21,7 @@ class AnalizadorLexico:
     
     def tokenizar(self, texto):
         buffer = ""
+        linha_atual = 1
         for linha in texto:
             for caractere in linha:
                 if(caractere != " "):
@@ -29,10 +30,50 @@ class AnalizadorLexico:
                 else:
                     #buffer = buffer.strip(" ")
                     if(self.palavras_reservadas.__contains__(buffer)):
-                        print("achou " + buffer)
-                        
+                        print("achou " + buffer + " "+ str(linha_atual))
+                        self.verifica_palavras_reservadas(buffer, linha_atual)
+
                     buffer = ""
             buffer = ""
                 #buffer = buffer + caractere if caractere != " " else buffer = ""
                     #print(buffer)
             #print(linha)
+            linha_atual += 1
+
+
+        for t in self.tokens:
+            print(t.nome + " " + t.lexema + " " + str(t.linha))
+
+    def verifica_palavras_reservadas(self, buffer, linha):
+        if (buffer == "main"):
+            self.tokens.append(TokenLex("<programa>","main",linha))
+            return True
+        elif (buffer == "int" or buffer == "boolean"):
+            self.tokens.append(TokenLex("<tipo>","int,boolean",linha))
+            return True
+        elif (buffer == "def"):
+            self.tokens.append(TokenLex("<declaracao_func>","def",linha))
+            return True
+        elif (buffer == "procedure"):
+            self.tokens.append(TokenLex("<procedimento>","proedure",linha))
+            return True
+        elif (buffer == "return"):
+            self.tokens.append(TokenLex("<retorno>","return",linha))
+            return True
+        elif (buffer == "if" or buffer == "else"):
+            self.tokens.append(TokenLex("<if>","if,else",linha))
+            return True
+        elif (buffer == "while"):
+            self.tokens.append(TokenLex("<laco>","while",linha))
+            return True
+        elif (buffer == "break"):
+            self.tokens.append(TokenLex("<parar>","break",linha))
+            return True
+        elif (buffer == "continue"):
+            self.tokens.append(TokenLex("<continuar>","continue",linha))
+            return True
+        elif (buffer == "printf"):
+            self.tokens.append(TokenLex("<imprimir>","printf",linha))
+            return True
+        else:
+            return False #não encontrado
