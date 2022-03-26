90678f828cf04459834b6d97b93bca3eb08bedbd
eemilyy <emilyalmeeidas@gmail.com>
2022-03-26 00:25:35 -0300

    fixing the function tokenizar

--- compiladorOO/analizador_lexico.py
+++ compiladorOO/analizador_lexico.py
@@ -5,11 +5,34 @@ class AnalizadorLexico:
     def __init__(self,texto):
         self.texto = texto
         self.tokens = []
-        self.palavras_reservadas= ["main"]
+        self.palavras_reservadas= ["main",
+                                    "int",
+                                    "boolean",
+                                    "const",
+                                    "def",
+                                    "procedure",
+                                    "return",
+                                    "if",
+                                    "else",
+                                    "while",
+                                    "break",
+                                    "continue", 
+                                    "printf"]
     
     def tokenizar(self, texto):
         buffer = ""
         for linha in texto:
             for caractere in linha:
-                print(caractere)
+                if(caractere != " "):
+                    buffer += caractere
+                    print(buffer)
+                else:
+                    #buffer = buffer.strip(" ")
+                    if(self.palavras_reservadas.__contains__(buffer)):
+                        print("achou " + buffer)
+                        
+                    buffer = ""
+            buffer = ""
+                #buffer = buffer + caractere if caractere != " " else buffer = ""
+                    #print(buffer)
             #print(linha)
