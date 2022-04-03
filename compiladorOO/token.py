914e3c3f05cc1187254a3a30be057d11869a38f9
eemilyy <emilyalmeeidas@gmail.com>
2022-04-03 19:18:32 -0300

    deleting token.py

--- compiladorOO/token.py
+++
@@ -1,8 +0,0 @@
-class TokenLex:
-    def __init__(self, nome, lexema, linha):
-        self.nome = nome
-        self.lexema = lexema
-        self.linha = linha
-
-    def imprimir(self):
-        print(self.nome + " " + self.lexema + " " + self.linha)
