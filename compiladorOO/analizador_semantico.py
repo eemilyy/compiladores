9f63005ab6db4daa5dd4f37ee3b4fb70214e9059
eemilyy <emilyalmeeidas@gmail.com>
2022-05-14 16:13:39 -0300

    inserindo mais formas de declarar variaveis

--- compiladorOO/analizador_semantico.py
+++ compiladorOO/analizador_semantico.py
@@ -1,10 +1,46 @@
-def get_tipo():
-    print("sj")
+def get_tipo(variavel, tabela_simbolos):
+    if variavel in tabela_simbolos:
+        #print(tabela_simbolos[variavel].tipo)
+        return tabela_simbolos[variavel].tipo
+
+
 def verificar_atribuicao(lista_tokens, tabela_simbolos, look_ahead):
     if lista_tokens[look_ahead - 2].lexema == "boolean" and lista_tokens[look_ahead + 1].nome == "<palavraBooleana>":
         return True
     elif lista_tokens[look_ahead - 2].lexema == "int" and lista_tokens[look_ahead + 1].nome == "<numerico>":
         return True
+    elif lista_tokens[look_ahead - 2].lexema == "int" and lista_tokens[look_ahead + 1].nome == "<variavel>":
+        tipo = get_tipo(lista_tokens[look_ahead + 1].lexema, tabela_simbolos)
+        #print(tipo)
+        if(tipo == "int"):
+            return True
+        else:
+            print('\033[91m' + "Semantic error in line {0}".format(lista_tokens[look_ahead - 2].linha) + '\033[0m') #DETALHAR ERRO
+            return False
+    # elif lista_tokens[look_ahead - 2].lexema == "boolean" and lista_tokens[look_ahead + 1].nome == "<variavel>":
+    #     tipo = get_tipo(lista_tokens[look_ahead + 1].lexema, tabela_simbolos)
+    #     #print(tipo)
+    #     if(tipo == "boolean"):
+    #         return True
+    #     else:
+    #         print('\033[91m' + "Semantic error in line {0}".format(lista_tokens[look_ahead - 2].linha) + '\033[0m') #DETALHAR ERRO
+    #         return False
+    elif lista_tokens[look_ahead - 2].nome == "<fim_comando>": # DECLARAÇÃO SEM SER DIRETA, EX: a = b;
+        print(lista_tokens[look_ahead + 1].lexema)
+        tipo = get_tipo(lista_tokens[look_ahead + 1].lexema, tabela_simbolos)
+        print(tipo)
+        if tipo == "int":
+            tipoAtribuicao = get_tipo(lista_tokens[look_ahead - 1].lexema, tabela_simbolos)
+            if tipoAtribuicao == tipo:
+                return True
+        elif tipo == "boolean":
+            tipoAtribuicao = get_tipo(lista_tokens[look_ahead - 1].lexema, tabela_simbolos)
+            print(tipoAtribuicao)
+            if tipoAtribuicao == tipo:
+                return True
+        print('\033[91m' + "Semantic error in line {0}".format(lista_tokens[look_ahead - 2].linha) + '\033[0m') #DETALHAR ERRO
+
+    
     else:
         #print("semantic error {}, expected {0} <--> {1}".format(lista_tokens[look_ahead - 2].lexema, lista_tokens[look_ahead + 1].lexema))
         print("entrou")
