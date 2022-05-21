444ac799117cca7381fbfb87a998158770ff5467
Michel Oliveira <micheltdoliveira@gmail.com>
2022-05-21 20:50:08 -0300

    add tabela de simbolo para funcoes

--- compiladorOO/simbolo.py
+++ compiladorOO/simbolo.py
@@ -3,3 +3,10 @@ class Simbolo:
         self.tipo = tipo
         self.linha = linha
 
+class SimboloFunc:
+
+    def __init__(self, tipo, linha, qtdParam, listParam):
+        self.tipo = tipo
+        self.linha = linha
+        self.qtdParam = qtdParam
+        self.listParam = listParam
